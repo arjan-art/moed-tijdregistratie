abc643d19d72356217e50614d8ef1df7547019c9
arjan-art <arjan@deterugwinning.nl>
2026-07-23 10:27:46 +0200

    Fix vite.config.ts - remove plugin-inspect-react-code

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
 import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
-import { inspectAttr } from 'plugin-inspect-react-code'
 
 export default defineConfig({
   base: './',
-  plugins: [inspectAttr(), react()],
+  plugins: [react()],
   server: {
     port: 3000,
   },
